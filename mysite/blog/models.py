819baaee5a8e5201dbc7ca44b589c5a6b5167ab9
Darshan107 <timilsinadarshan107@gmail.com>
2024-09-17 21:02:25 +0545

    added author attr and migrated Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -8,6 +9,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
